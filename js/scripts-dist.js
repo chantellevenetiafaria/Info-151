b1e8696b283511439ea723b07262cfacd391fed8
Chantelle Faria <109173028+chantellevenetiafaria@users.noreply.github.com>
2023-12-15 10:04:20 -0500

    made changes

--- js/scripts-dist.js
+++ js/scripts-dist.js
@@ -1,1 +1,14 @@
-var mainNav=document.querySelector("#main-nav"),signInContainer=document.querySelector("#signIn-container"),burgerBtn=document.querySelector("#burger-container button"),canYouSeeMainNav=!1;function showNav(){!1===canYouSeeMainNav?(console.log("show menu"),mainNav.classList.add("showMainNav"),signInContainer.classList.add("showSignIn"),canYouSeeMainNav=!0):(console.log("hide menu"),mainNav.classList.remove("showMainNav"),signInContainer.classList.remove("showSignIn"),canYouSeeMainNav=!1)}burgerBtn.addEventListener("click",showNav);
+var mainNav = document.querySelector("#main-nav")
+  , signInContainer = document.querySelector("#signIn-container")
+  , burgerBtn = document.querySelector("#burger-container button")
+  , canYouSeeMainNav = !1;
+function showNav() {
+    !1 === canYouSeeMainNav ? (console.log("show menu"),
+    mainNav.classList.add("showMainNav"),
+    signInContainer.classList.add("showSignIn"),
+    canYouSeeMainNav = !0) : (console.log("hide menu"),
+    mainNav.classList.remove("showMainNav"),
+    signInContainer.classList.remove("showSignIn"),
+    canYouSeeMainNav = !1)
+}
+burgerBtn.addEventListener("click", showNav);
