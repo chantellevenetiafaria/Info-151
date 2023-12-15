20adefbd05187be86c628cb337be2dde283311a7
Chantelle Faria <109173028+chantellevenetiafaria@users.noreply.github.com>
2023-12-15 12:34:01 -0500

    Updated Style

--- js/scripts.js
+++ js/scripts.js
@@ -1,109 +1,93 @@
-/*------------------------------
-      Navigation
--------------------------------*/
+Fancybox.bind();
+
+// const container = document.getElementById("myCarousel");
+const container = document.querySelector("#myCarousel");
+
+const options = { infinite: true };
+
+new Carousel(container, options);
 
 
-var mainNav = document.querySelector("#mainNav");
 
-var burgerBtn = document.querySelector("#burger-container button");
 
+/* --------------------
+    Navigation 
+ -------------------- */  
+
+var mainNav = document.querySelector("#main-nav");
 var signInUpButtons = document.querySelector("#sign-in-up-buttons");
+var burgerBtn = document.querySelector("#burger-container button");
 
 var burgerBars = document.querySelector("#burger-bars");
 var burgerX = document.querySelector("#burger-x");
 
+
+// this can only be true or false known as a boolean
 var canYouSeeMainNav = false;
 
-//console.log(burgerBtn);
-
-console.log("working");
-//var sectionTags = document.querySelectorAll("section");
-
-
-
 function showNav(){
-    //console.log("button clicked");
+    // console.log("button clicked");
     if(canYouSeeMainNav === false){
         console.log("show menu");
-        //this will add the class of showMainNav to the id mainNav
+        // this will add the class of showMainNav to the id mainNav
         mainNav.classList.add("showMainNav");
 
         signInUpButtons.classList.add("showSignIn");
-
         canYouSeeMainNav = true;
 
+        // hide burger bars
         burgerBars.classList.add("hide-burger-bars");
 
+        // show burger x
         burgerX.classList.remove("hide-burger-x");
+
     }else{
         console.log("hide menu");
+        // this will remove the class of showMainNav to the id mainNav
         mainNav.classList.remove("showMainNav");
-        //this will remove the class of showMainNav to the id mainNav
+
         signInUpButtons.classList.remove("showSignIn");
+
+        // show burger bars
         burgerBars.classList.remove("hide-burger-bars");
 
+        // hide burger x
         burgerX.classList.add("hide-burger-x");
+
         canYouSeeMainNav = false;
     }
     
-
 }
 
-//add a event listener
+// add a event listener to the button
 burgerBtn.addEventListener("click", showNav);
 
 
-//will call a function
-//showNav();
 
-/*------------------------------
-      Sign In
--------------------------------*/
-
-var signInBtn = document.querySelector("#sign-in-btn");
-
-var signInContainer = document.querySelector("#sign-in-container");
-
-var closeSignUpModalBtn = document.querySelector("#close-sign-up-modal-btn");
+/* --------------------
+    Sign In 
+ -------------------- */  
+ var signInBtn = document.querySelector("#sign-in-btn");
+ var signInContainer = document.querySelector("#sign-in-container");
+ var closeSignUpModalBtn = document.querySelector("#close-sign-up-modal-btn");
 
 
-function showSignInModal(){
-    //console.log("show modal now");
-
+ function showSignInModal() {
+    // console.log("show modal now");
     signInContainer.classList.add("show-signIn-container");
+    // hide the navigation on close
     mainNav.classList.remove("showMainNav");
+    // hide the signup/in buttons
     signInUpButtons.classList.remove("showSignIn");
-}
+ }
 
-function hideSignInModal(){
+ function hideSignInModal(){
     signInContainer.classList.remove("show-signIn-container");
-}
+    
+ }
 
-signInBtn.addEventListener("click",showSignInModal);
-closeSignUpModalBtn.addEventListener("click",hideSignInModal);
+ signInBtn.addEventListener("click", showSignInModal);
+ closeSignUpModalBtn.addEventListener("click", hideSignInModal);
 
-/*------------------------------
-      Sign Up
--------------------------------*/
-
-var signUpBtn = document.querySelector("#sign-up-btn");
-
-var signUpContainer = document.querySelector("#sign-up-container");
-
-var closeSignUpModalBtn = document.querySelector("#close-sign-up-modal-btn");
-
-
-function showSignUpModal(){
-    //console.log("show modal now");
-
-    signUpContainer.classList.add("show-signUp-container");
-    mainNav.classList.remove("showMainNav");
-    signInUpButtons.classList.remove("showSignUp");
-}
-
-function hideSignUpModal(){
-    signUpContainer.classList.remove("show-signUp-container");
-}
-
-signUpBtn.addEventListener("click",showSignUpModal);
-closeSignUpModalBtn.addEventListener("click",hideSignUpModal);
+//  signInContainer.addEventListener("click", hideSignInModal);
+ 
