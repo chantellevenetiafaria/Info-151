429e1465c6250489bb3ff0656f2c59c694b07b6f
Chantelle Faria <109173028+chantellevenetiafaria@users.noreply.github.com>
2023-12-15 10:12:45 -0500

    week 8 sign in form

--- js/scripts-dist.js
+++ js/scripts-dist.js
@@ -1,14 +1,50 @@
-var mainNav = document.querySelector("#main-nav")
-  , signInContainer = document.querySelector("#signIn-container")
-  , burgerBtn = document.querySelector("#burger-container button")
-  , canYouSeeMainNav = !1;
+var mainNav = document.querySelector("#main-nav");
+var signInUpButtons = document.querySelector("#sign-in-up-buttons");
+var burgerBtn = document.querySelector("#burger-container button");
+var burgerBars = document.querySelector("#burger-bars");
+var burgerX = document.querySelector("#burger-x");
+var canYouSeeMainNav = false;
+
 function showNav() {
-    !1 === canYouSeeMainNav ? (console.log("show menu"),
-    mainNav.classList.add("showMainNav"),
-    signInContainer.classList.add("showSignIn"),
-    canYouSeeMainNav = !0) : (console.log("hide menu"),
-    mainNav.classList.remove("showMainNav"),
-    signInContainer.classList.remove("showSignIn"),
-    canYouSeeMainNav = !1)
+    if (!canYouSeeMainNav) {
+        console.log("show menu");
+        mainNav.classList.add("showMainNav");
+        signInUpButtons.classList.add("showSignIn");
+        canYouSeeMainNav = true;
+        burgerBars.classList.add("hide-burger-bars");
+        burgerX.classList.remove("hide-burger-x");
+    } else {
+        console.log("hide menu");
+        mainNav.classList.remove("showMainNav");
+        signInUpButtons.classList.remove("showSignIn");
+        burgerBars.classList.remove("hide-burger-bars");
+        burgerX.classList.add("hide-burger-x");
+        canYouSeeMainNav = false;
+    }
 }
+
 burgerBtn.addEventListener("click", showNav);
+
+var signInBtn = document.querySelector("#sign-in-btn");
+var signInContainer = document.querySelector("#sign-in-container");
+var closeSignUpModalBtn = document.querySelector("#close-sign-up-modal-btn");
+
+function showSignInModal() {
+    signInContainer.classList.add("show-signIn-container");
+    mainNav.classList.remove("showMainNav");
+    signInUpButtons.classList.remove("showSignIn");
+}
+
+function hideSignInModal() {
+    signInContainer.classList.remove("show-signIn-container");
+}
+
+signInBtn.addEventListener("click", showSignInModal);
+closeSignUpModalBtn.addEventListener("click", hideSignInModal);
+
+document.addEventListener("click", function (event) {
+    // Check if the click is outside signInContainer
+    if (!signInContainer.contains(event.target)) {
+        hideSignInModal();
+    }
+});
